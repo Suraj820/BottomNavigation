03f41185511a6135ac82aa1ffec3bcbd40a1a6a9
Suraj820 <king007suraj@gmail.com>
2022-03-10 10:37:52 +0530

    Initial commit

---
+++ app/src/main/java/com/java/bottomnavigation/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.java.bottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.java.bottomnavigation.R
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_home,R.id.nav_tickets,R.id.nav_offers,R.id.nav_rewards))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        findViewById<BottomNavigationView>(R.id.nav_view)?.setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/com/java/bottomnavigation/fragmnets/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.java.bottomnavigation.fragmnets
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.java.bottomnavigation.R
+
+
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        view.findViewById<Button>(R.id.button_home)?.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.nav_home_to_content, null)
+        )
+
+        return view
+    }
+
+
+}
